9abab5ae5bf9dffb0b788c4f7c028953fea91215
Dido <davide-neri@di.unipi.it>
2019-04-09 20:33:54 +0200

    types for communicaton pattern

--- microtosca-client/src/app/model/type.ts
+++ microtosca-client/src/app/model/type.ts
@@ -1,6 +1,6 @@
 export class ConcreteTypes {
     public static readonly MESSAGE_BROKER = "MessageBroker";
-    public static readonly API_GATEWAY = "ApiGateway";
+    public static readonly API_GATEWAY = "ApiGatembway";
     // public static readonly MESSAGE_ROUTER = "MessageRouter";
     public static readonly CIRCUIT_BREAKER = "CircuitBreaker";
   }
