c4b76fe5e8f6d1f4c782675a7ec25e9111e8dbc2
Dido <davide-neri@di.unipi.it>
2019-05-08 10:07:12 +0200

    created client and server folder

--- server/microanalyser-server/urls.py
+++ server/microanalyser-server/urls.py
@@ -24,30 +24,10 @@ from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='MicroToscaAnalyser API')
 
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-
-
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # url(r'^', include(router.urls)),
     url(r'^$', schema_view),
-    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('v1/', include('api.urls')),
+    # path('v1/', include('api.urls')),
     path('v2/', include('api.v2urls'))
 ]
