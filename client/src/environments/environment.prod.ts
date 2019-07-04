3bf6a2c3e5996633174528291f4929db717eb203
Davide <davide.neri@di.unipi.it>
2019-07-04 10:07:48 +0200

    Update environment.prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // serverUrl: "http://neri.di.unipi.it:8000"
-  serverUrl: "http://api.microfreshener.localhost"
+  serverUrl: "http://neri.di.unipi.it:8000"
+  //serverUrl: "http://api.microfreshener.localhost"
 };
