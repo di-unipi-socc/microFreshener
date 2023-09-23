0e922cba258a97047f54789036f09fc636cdb3f0
meek <agomik@gmail.com>
2023-09-23 02:10:13 +0200

    UI for login

---
+++ client/src/app/core/login-page/login-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent {
+
+  constructor() {
+
+  }
+
+  login() {
+    console.log('login');
+  }
+
+}
