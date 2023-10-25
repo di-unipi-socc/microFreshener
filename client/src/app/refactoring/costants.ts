9d817072e09ff34f5f37d0e30130b5005ece1cbd
meek <agomik@gmail.com>
2023-10-25 16:20:56 +0200

    Core's SplitTeamsByService is now multiplexed in frontend

--- client/src/app/refactoring/costants.ts
+++ client/src/app/refactoring/costants.ts
@@ -7,18 +7,16 @@ export enum SMELL_NAMES {
     SMELL_MULTIPLE_SERVICES_IN_ONE_CONTAINER = "Multiple-services-in-one-container"
 }
 
+
 export enum REFACTORING_NAMES {
-    REFACTORING_ADD_SERVICE_DISCOVERY = 'Add-service-discovery',
-    REFACTORING_ADD_MESSAGE_ROUTER = 'Add-message-router',
-    REFACTORING_ADD_MESSAGE_BROKER = 'Add-message-broker',
-    REFACTORING_ADD_CIRCUIT_BREAKER = 'Add-circuit-breaker',
-    REFACTORING_USE_TIMEOUT = "Use-timeout",
-    REFACTORING_MERGE_SERVICES = "Merge-service",
-    REFACTORING_SPLIT_DATASTORE = "Split-Datastore",
-    REFACTORING_ADD_DATA_MANAGER = "Add-data-manager",
-    REFACTORING_ADD_API_GATEWAY = "Add-api-gateway",
-    REFACTORING_ADD_TEAM_DATA_MANAGER = "Add-data-team-data-manager",
-    REFACTORING_CHANGE_DATASTORE_OWENRSHIP = "Change-Datastore-ownership",
-    REFACTORING_CHANGE_SERVICE_OWENRSHIP = "Change-service-ownership",
-    REFACTORING_MERGE_TEAMS = "Merge-teams"
+  REFACTORING_ADD_SERVICE_DISCOVERY = 'Add-service-discovery',
+  REFACTORING_ADD_MESSAGE_ROUTER = 'Add-message-router',
+  REFACTORING_ADD_MESSAGE_BROKER = 'Add-message-broker',
+  REFACTORING_ADD_CIRCUIT_BREAKER = 'Add-circuit-breaker',
+  REFACTORING_USE_TIMEOUT = "Use-timeout",
+  REFACTORING_MERGE_SERVICES = "Merge-service",
+  REFACTORING_SPLIT_DATASTORE = "Split-Datastore",
+  REFACTORING_ADD_DATA_MANAGER = "Add-data-manager",
+  REFACTORING_ADD_API_GATEWAY = "Add-api-gateway",
+  REFACTORING_SPLIT_TEAMS_BY_SERVICE = "Split-teams-by-service"
 }
