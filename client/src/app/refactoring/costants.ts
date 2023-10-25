746fa14c3168e79d1314fc0fb0c59e48c1553464
meek <agomik@gmail.com>
2023-10-25 18:51:04 +0200

    Constants for TightlyCoupledTeamsSmell added

--- client/src/app/refactoring/costants.ts
+++ client/src/app/refactoring/costants.ts
@@ -4,7 +4,8 @@ export enum SMELL_NAMES {
     SMELL_SHARED_PERSISTENCY = "Shared-persistency",
     SMELL_NO_API_GATEWAY = "No-api-gateway",
     SMELL_SINGLE_LAYER_TEAMS = "Single-layer-teams",
-    SMELL_MULTIPLE_SERVICES_IN_ONE_CONTAINER = "Multiple-services-in-one-container"
+    SMELL_MULTIPLE_SERVICES_IN_ONE_CONTAINER = "Multiple-services-in-one-container",
+    SMELL_TIGHTLY_COUPLED_TEAMS = "Tightly-coupled-teams"
 }
 
 
@@ -18,5 +19,6 @@ export enum REFACTORING_NAMES {
   REFACTORING_SPLIT_DATASTORE = "Split-Datastore",
   REFACTORING_ADD_DATA_MANAGER = "Add-data-manager",
   REFACTORING_ADD_API_GATEWAY = "Add-api-gateway",
-  REFACTORING_SPLIT_TEAMS_BY_SERVICE = "Split-teams-by-service"
+  REFACTORING_SPLIT_TEAMS_BY_SERVICE = "Split-teams-by-service",
+  REFACTORING_SPLIT_TEAMS_BY_COUPLING = "Split-teams-by-coupling"
 }
