1f57480f6c0e1a8eeb99def1e5b5eae4868dac3b
meek <agomik@gmail.com>
2024-04-17 12:51:40 +0200

    Teams, computes, SLT and refactoring aligned with last definition

--- client/src/app/editor/graph-editor.component.ts
+++ client/src/app/editor/graph-editor.component.ts
@@ -419,7 +419,7 @@ export class GraphEditorComponent {
         this.navigation.getPaper().on("element:pointerclick", (cellView, evt, x, y) => {
             console.debug("clicked", cellView);
             console.log("click on cell");
-            if(this.deployments.isCompute(cellView.model) || this.deployments.isDeploymentLink(cellView.model)) {
+            if(this.deployments.isDeploymentLink(cellView.model)) {
                 if(this.leftClickSelectedCell)
                     this.stopAddingLink();
                 return;
@@ -536,7 +536,7 @@ export class GraphEditorComponent {
 
     bindTeamEmbedNodes() {
         this.navigation.getPaper().on('cell:pointerdown', ((cellView, evt, x, y) => {
-            if(!this.addingLink && this.architecture.isNode(cellView.model) && !this.deployments.isCompute(cellView.model)) {
+            if(!this.addingLink && this.architecture.isNode(cellView.model)) {
                 let node = cellView.model;
                 this.draggingNode = true;
                 let team = this.teams.getTeamOfNode(node);
